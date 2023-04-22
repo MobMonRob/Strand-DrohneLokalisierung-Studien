8a11ab99238555f23e409410f644d8fbd852a5bc
Maximilian Burr <maximilian.burr@outlook.de>
2023-04-22 15:36:16 +0200

    updated slam and added reference

---
+++ Ausarbeitung/bericht.for
@@ -0,0 +1,3 @@
+\contentsline {eq}{(\ref {eq:transform-matrix}) Transformationsmatix}{28}{section*.26}%
+\contentsline {eq}{(\ref {eq:intrinsic-matrix}) Intrinsische Maxtrix}{29}{figure.3.4}%
+\contentsline {eq}{(\ref {eq:extrinsic-matrix}) Extrinsische Matrix}{29}{figure.3.4}%
