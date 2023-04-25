6250ecba06f495ebbe7ebe7ba7fb81502817c6f6
Maximilian Burr <maximilian.burr@outlook.de>
2023-04-25 16:22:32 +0200

    merge

--- Ausarbeitung/bericht.for
+++ Ausarbeitung/bericht.for
@@ -1,3 +1,3 @@
-\contentsline {eq}{(\ref {eq:transform-matrix}) Transformationsmatix}{28}{section*.26}%
-\contentsline {eq}{(\ref {eq:intrinsic-matrix}) Intrinsische Maxtrix}{29}{figure.3.4}%
-\contentsline {eq}{(\ref {eq:extrinsic-matrix}) Extrinsische Matrix}{29}{figure.3.4}%
+\contentsline {eq}{(\ref {eq:transform-matrix}) Transformationsmatix}{28}{section*.22}%
+\contentsline {eq}{(\ref {eq:intrinsic-matrix}) Intrinsische Maxtrix}{28}{figure.3.1}%
+\contentsline {eq}{(\ref {eq:extrinsic-matrix}) Extrinsische Matrix}{29}{figure.3.1}%
