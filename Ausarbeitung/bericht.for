404de64081d9ed8d14974563fa4204acefa46aa3
Johannes V <vater.jo@gmail.com>
2023-04-25 18:22:41 +0200

    .

--- Ausarbeitung/bericht.for
+++ Ausarbeitung/bericht.for
@@ -1,3 +1,3 @@
-\contentsline {eq}{(\ref {eq:transform-matrix}) Transformationsmatix}{31}{section*.28}%
+\contentsline {eq}{(\ref {eq:transform-matrix}) Transformationsmatix}{32}{section*.29}%
 \contentsline {eq}{(\ref {eq:intrinsic-matrix}) Intrinsische Maxtrix}{32}{figure.3.1}%
-\contentsline {eq}{(\ref {eq:extrinsic-matrix}) Extrinsische Matrix}{32}{figure.3.1}%
+\contentsline {eq}{(\ref {eq:extrinsic-matrix}) Extrinsische Matrix}{33}{figure.3.1}%
