cea7c80c19d1ab817c2ffe8fbb88758975dfcd39
Maximilian Burr <maximilian.burr@outlook.de>
2023-04-25 18:29:46 +0200

    add pid regler

--- Ausarbeitung/bericht.for
+++ Ausarbeitung/bericht.for
@@ -1,3 +1,3 @@
-\contentsline {eq}{(\ref {eq:transform-matrix}) Transformationsmatix}{28}{section*.24}%
-\contentsline {eq}{(\ref {eq:intrinsic-matrix}) Intrinsische Maxtrix}{28}{figure.3.1}%
-\contentsline {eq}{(\ref {eq:extrinsic-matrix}) Extrinsische Matrix}{29}{figure.3.1}%
+\contentsline {eq}{(\ref {eq:transform-matrix}) Transformationsmatix}{29}{section*.25}%
+\contentsline {eq}{(\ref {eq:intrinsic-matrix}) Intrinsische Maxtrix}{29}{figure.3.1}%
+\contentsline {eq}{(\ref {eq:extrinsic-matrix}) Extrinsische Matrix}{30}{figure.3.1}%
