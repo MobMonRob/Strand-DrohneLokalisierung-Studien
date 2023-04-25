739ed4ad55b84ee80c1535ced3a997a04d26c6f3
Johannes V <vater.jo@gmail.com>
2023-04-25 18:20:40 +0200

    add more to 3d scanner

--- Ausarbeitung/bericht.for
+++ Ausarbeitung/bericht.for
@@ -1,3 +1,3 @@
-\contentsline {eq}{(\ref {eq:transform-matrix}) Transformationsmatix}{29}{section*.28}%
-\contentsline {eq}{(\ref {eq:intrinsic-matrix}) Intrinsische Maxtrix}{30}{figure.3.1}%
-\contentsline {eq}{(\ref {eq:extrinsic-matrix}) Extrinsische Matrix}{30}{figure.3.1}%
+\contentsline {eq}{(\ref {eq:transform-matrix}) Transformationsmatix}{31}{section*.28}%
+\contentsline {eq}{(\ref {eq:intrinsic-matrix}) Intrinsische Maxtrix}{32}{figure.3.1}%
+\contentsline {eq}{(\ref {eq:extrinsic-matrix}) Extrinsische Matrix}{32}{figure.3.1}%
