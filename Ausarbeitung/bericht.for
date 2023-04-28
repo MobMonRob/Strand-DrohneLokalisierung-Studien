6f77c0892ca3dd75fcdf31db194d88c972014966
Maximilian Burr <maximilian.burr@outlook.de>
2023-04-28 14:06:29 +0200

    typo fixes and moved kalman filter chapter  and created flight stack chapter

--- Ausarbeitung/bericht.for
+++ Ausarbeitung/bericht.for
@@ -1,3 +1,3 @@
-\contentsline {eq}{(\ref {eq:transform-matrix}) Transformationsmatix}{32}{section*.29}%
+\contentsline {eq}{(\ref {eq:transform-matrix}) Transformationsmatix}{32}{section*.27}%
 \contentsline {eq}{(\ref {eq:intrinsic-matrix}) Intrinsische Maxtrix}{33}{figure.3.1}%
 \contentsline {eq}{(\ref {eq:extrinsic-matrix}) Extrinsische Matrix}{33}{figure.3.1}%
