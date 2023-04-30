d419cc4666a449cda810352cc22a0b0a2b22de77
Maximilian Burr <maximilian.burr@outlook.de>
2023-04-30 15:56:43 +0200

    abk.tex

--- Ausarbeitung/bericht.for
+++
@@ -1,3 +0,0 @@
-\contentsline {eq}{(\ref {eq:transform-matrix}) Transformationsmatix}{32}{section*.27}%
-\contentsline {eq}{(\ref {eq:intrinsic-matrix}) Intrinsische Maxtrix}{33}{figure.3.1}%
-\contentsline {eq}{(\ref {eq:extrinsic-matrix}) Extrinsische Matrix}{33}{figure.3.1}%
